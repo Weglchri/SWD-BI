b8395e6ae8f6842f38bf6c1df176ee3fd671bf7d
Weglchri15 <christopher.wegl@edu.fh-joanneum.at>
2016-10-18 21:44:51 +0200

    Completed SQL Create & Drop

---
+++ BusinessIntelligence/sql/Drop.sql
@@ -0,0 +1,9 @@
+DROP TABLE public.Responsibility CASCADE
+DROP TABLE public.Project CASCADE
+DROP TABLE public.Offer CASCADE
+DROP TABLE public.Location CASCADE
+DROP TABLE public.Company CASCADE
+DROP TABLE public.ProjectManager CASCADE
+DROP TABLE public.Freelancer CASCADE
+DROP TABLE public.User CASCADE
+
